a0b1d6d088abbdfb53d67a2fd3a89c6d7c60b862
tucker <tucker@naver.com>
2021-01-11 11:57:28 -0800

    ex33

---
+++ ex33.1/ex33.1.go
@@ -0,0 +1,21 @@
+//ex33.1//ex33.1.go
+package main
+
+import "fmt"
+
+func main() {
+	var array [5]int = [5]int{1, 2, 3, 4, 5}
+	var b [5]int
+	b = array // ❶
+
+	var c []int
+	// c = array 는 안된다.
+	c = array[:] // ❷
+
+	b[0] = 1000 // ❸
+	c[3] = 500
+
+	fmt.Println("array:", array)
+	fmt.Println("b:", b)
+	fmt.Println("c:", c)
+}
---
+++ ex33.2/ex33.2.go
@@ -0,0 +1,20 @@
+//ex33.2//ex33.2.go
+package main
+
+import "fmt"
+
+func CallbyCopy(n int, b [5]int, s []int) {
+	n = 3000
+	b[0] = 1000
+	s[3] = 500
+}
+
+func main() {
+	var array [5]int = [5]int{1, 2, 3, 4, 5}
+	var c []int
+	c = array[:]
+	CallbyCopy(100, array, c) // ❶
+
+	fmt.Println("array:", array)
+	fmt.Println("c:", c)
+}

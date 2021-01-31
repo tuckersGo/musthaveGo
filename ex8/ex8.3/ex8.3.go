112fb7a336e00ada1444e6e1acbf58d3580143a0
tucker <tucker@naver.com>
2021-01-31 15:36:50 -0800

    change ex8.3

--- ex8/ex8.3/ex8.3.go
+++ ex8/ex8.3/ex8.3.go
@@ -1,33 +1,29 @@
 //ex8/ex8.3/ex8.3.go
 package main
 
-import (
-	"fmt"
-
-	"github.com/fatih/color"
-)
+import "fmt"
 
 // ❶ 상수값에 코드를 부여합니다.
-const Red int = 0
-const Blue int = 1
-const Green int = 2
+const Pig int = 0
+const Cow int = 1
+const Chicken int = 2
 
-// ❸ 코드값에 따라서 다른 색깔의 텍스트를 출력합니다.
-func PrintColor(clr int, text string) {
-	if clr == Red {
-		color.Red(text)
-	} else if clr == Blue {
-		color.Blue(text)
-	} else if clr == Green {
-		color.Green(text)
+// ❸ 코드값에 따라서 다른 텍스트를 출력합니다.
+func PrintAnimal(animal int) {
+	if animal == Pig {
+		fmt.Println("꿀꿀")
+	} else if animal == Cow {
+		fmt.Println("음메")
+	} else if animal == Chicken {
+		fmt.Println("꼬끼오")
 	} else {
-		fmt.Println(text)
+		fmt.Println("...")
 	}
 }
 
 func main() {
-	// ❷ PrintColor() 함수를 호출하여 색깔있는 글자를 출력합니다.
-	PrintColor(Red, "Prints text in red.")
-	PrintColor(Blue, "Prints text in blue.")
-	PrintColor(Green, "Prints text in green.")
+	// ❷ PrintAnimal() 함수를 호출하여 동물 소리를 출력합니다.
+	PrintAnimal(Pig)
+	PrintAnimal(Cow)
+	PrintAnimal(Chicken)
 }
